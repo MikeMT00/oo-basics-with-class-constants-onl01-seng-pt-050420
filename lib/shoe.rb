ea2eed370e20573090e17ad774a06e1db40f984f
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-25 20:54:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
+    if !(BRANDS.include?(@brand)) #if BRANDS array is not inclueded in @brand 
       BRANDS << brand
     end
   end
